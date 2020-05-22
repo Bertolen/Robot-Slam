32c64ed6ae302bbdf5f8ec8ac5f2880473b566dc
Bertolen <33809187+Bertolen@users.noreply.github.com>
2020-05-22 21:09:53 +0200

    Le robot bouge maintenant

--- src/environnement/robot/Robot.java
+++ src/environnement/robot/Robot.java
@@ -1,19 +1,26 @@
 package environnement.robot;
 
 import java.awt.Point;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.geom.Point2D;
+
+import javax.swing.Timer;
 
 public class Robot {
 
-	Point location;
+	Point2D.Double location;
 	Point2D.Double orientation;
 	double taille;
-	double vitesse;
+	double vitesseLin;
+	double vitesseRot;
+	double lastTick;
 	
 	public Robot() {
 		taille = 30.0d;
-		vitesse = 30.0d;
-		location = new Point(-100, -100);
+		vitesseLin = 30.0d;
+		vitesseRot = 0.01d;
+		location = new Point2D.Double(-100, -100);
 		orientation = new Point2D.Double(0, 1);
 	}
 
@@ -31,7 +38,7 @@ public class Robot {
 		orientation.setLocation(alpha / norme, beta / norme);
 	}
 	
-	public Point getLocation() {
+	public Point2D.Double getLocation() {
 		return location;
 	}
 	
@@ -46,6 +53,51 @@ public class Robot {
 	////////////////////////////////////// Méthodes //////////////////////////////////////
 	
 	public void lancer() {
-		System.out.println("Lancer le robot");
+
+		// Initialisation du Timer pour les Tick du robot
+		Timer timer = new Timer(50, new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				Tick();
+			}
+		});
+		timer.start();
+		lastTick = System.currentTimeMillis();
+	}
+	
+	private void Tick() {
+		double delta = getDeltaTime();
+		
+		// Mise à jour de l'IA
+		DecisionUpdate(delta);
+		
+		// Mise à jour de l'emplacement
+		PhysicsUpdate(delta);
+	}
+	
+	//Calcul du temps écoulé depuis le dernier tick, retour de cette valeur et mise à jour
+	private double getDeltaTime() {
+		double currentTick = System.currentTimeMillis();
+		double delta = currentTick - lastTick;
+		lastTick = currentTick;
+		return delta;
+	}
+	
+	// Prise de décision du robot (vitesse linéaire et de rotation)
+	// C'est la fonction principale de ce projet
+	protected void DecisionUpdate(double delta) {
+		// TODO
+	}
+	
+	// Calcul du nouvel emplacement du robot
+	protected void PhysicsUpdate(double delta) {
+		
+		// On commence par appliquer la rotation
+		double newAngle = Math.atan2(orientation.getY(), orientation.getX()) + vitesseRot;
+		orientation.setLocation(Math.cos(newAngle), Math.sin(newAngle));
+		
+		// On finis en appliquant la translation
+		double newX = location.getX() + orientation.getX() * vitesseLin * delta / 1000;
+		double newY = location.getY() + orientation.getY() * vitesseLin * delta / 1000;
+		location.setLocation(newX, newY);
 	}
 }
